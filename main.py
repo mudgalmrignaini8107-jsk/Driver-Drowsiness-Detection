d3bcfe4f9be3ea30f0553787045e60514ac1442b
Mrignaini <mudgalmrignaini8107@gmail.com>
2026-07-01 23:05:05 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import cv2
+import mediapipe as mp
+
+mp_face_mesh = mp.solutions.face_mesh
+face_mesh = mp_face_mesh.FaceMesh(refine_landmarks=True)
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    success, frame = camera.read()
+
+    if not success:
+        break
+
+    frame = cv2.flip(frame, 1)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = face_mesh.process(rgb)
+
+    if results.multi_face_landmarks:
+        for face_landmarks in results.multi_face_landmarks:
+            for landmark in face_landmarks.landmark:
+                h, w, _ = frame.shape
+                x = int(landmark.x * w)
+                y = int(landmark.y * h)
+
+                cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+
+    cv2.imshow("Face Mesh", frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
